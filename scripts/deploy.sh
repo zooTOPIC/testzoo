2e0f52ce61e256a578ef1ee232bed0948a6779d2
KYSgh <162289300+KYSgh@users.noreply.github.com>
2024-03-20 15:23:53 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,10 +20,3 @@ fi
 
 echo "> $JAR_PATH 배포"
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
-
-
-
-
-cd /home/ec2-user/app
-sudo docker build -t tcat-api-spring-boot-docker .
-sudo docker run -d -p 8080:8080 tcat-api-spring-boot-docker
