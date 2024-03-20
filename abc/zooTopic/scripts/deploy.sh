a1b2ea7636f4d4452e23be5eda283398309da5ac
KYSgh <162289300+KYSgh@users.noreply.github.com>
2024-03-20 15:16:13 +0900

    Update deploy.sh

--- abc/zooTopic/scripts/deploy.sh
+++ abc/zooTopic/scripts/deploy.sh
@@ -1,3 +1,35 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ec2-user/cicdproject 
+cd $REPOSITORY
+
+APP_NAME=cicdproject
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할것 없음."
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+
+
+
+
+
+
+
+
+
 #!/bin/bash
 
 echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
