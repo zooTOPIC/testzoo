6f33cd1b72428f8849a37efe685d00af444e17f5
KYSgh <162289300+KYSgh@users.noreply.github.com>
2024-03-20 15:17:02 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ec2-user/cicdproject
+REPOSITORY=/home/ec2-user/cicdproject 
 cd $REPOSITORY
 
 APP_NAME=cicdproject
@@ -20,3 +20,23 @@ fi
 
 echo "> $JAR_PATH 배포"
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+
+
+#!/bin/bash
+
+echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
+CURRENT_PID=$(sudo docker container ls -q)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 Docker 컨테이너가 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
+else
+  echo "> sudo docker stop $CURRENT_PID"   # 현재 구동중인 Docker 컨테이너가 있다면 모두 중지
+  sudo docker stop $CURRENT_PID
+  sleep 5
+fi
+
+cd /home/ec2-user/app
+sudo docker build -t tcat-api-spring-boot-docker .
+sudo docker run -d -p 8080:8080 tcat-api-spring-boot-docker
