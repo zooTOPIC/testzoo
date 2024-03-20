4aec4cbf8706bcf0715f4b40ba63411100635cfa
KYSgh <162289300+KYSgh@users.noreply.github.com>
2024-03-20 18:54:04 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,28 +1,35 @@
-!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/practice-githubAction-deploy-0.0.1-SNAPSHOT.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-
-echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
-
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/action/deploy.log
-
-echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
-DEPLOY_PATH=/home/ubuntu/action/
-cp $BUILD_JAR $DEPLOY_PATH
-
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
-
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
-else
-  echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
-  sudo kill -9 $CURRENT_PID
-  sleep 5
-fi
-
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
 sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+
+
+#!/bin/bash
+
+cd /home/ec2-user/app
+
+DOCKER_APP_NAME=spring
+
+# 실행중인 blue가 있는지
+EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep running)
+
+# green이 실행중이면 blue up
+if [ -z "$EXIST_BLUE" ]; then
+	echo "blue up"
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
+
+	sleep 30
+
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
+	docker image prune -af # 사용하지 않는 이미지 삭제
+
+# blue가 실행중이면 green up
+else
+	echo "green up"
+	docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
+
+	sleep 30
+
+	docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
+	docker image prune -af
+fi
